ea20d57606ad6a20edc8a3837279d8f127ed835a
Christopher Simpson <git@nilk.net>
2019-12-07 10:49:08 -0700

    Change python round to use precision paramter

--- python_code/helpers/numbers.py
+++ python_code/helpers/numbers.py
@@ -3,8 +3,8 @@ def pow_and_fix(f_value, decimals, offset=1):
     precision = len(s_value) - (s_value.find('.') + 1)
     stage_a = round(f_value * pow(10, precision))
     stage_b = pow(10, decimals - precision)
-    stage_c = (stage_a * stage_b) * pow(10, offset)
-    result = round(stage_c) / pow(10, offset)
+    stage_c = (stage_a * stage_b)
+    result = round(stage_c, offset)
     print('f_value', f_value, 'decimals', decimals, 'offset', offset,
           's_value', s_value, 'precision', precision,
           'stage_a', stage_a, 'stage_b', stage_b, 'stage_c', stage_c,
